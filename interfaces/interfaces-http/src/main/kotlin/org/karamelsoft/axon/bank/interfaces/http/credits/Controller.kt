5ec1e9b1d17a6e354095a0594455dbb5ad20bfe4
gpuget <gpuget@excilys.com>
2022-10-20 16:53:40 +0200

    Credit line history

---
+++ interfaces/interfaces-http/src/main/kotlin/org/karamelsoft/axon/bank/interfaces/http/credits/Controller.kt
@@ -0,0 +1,22 @@
+package org.karamelsoft.axon.bank.interfaces.http.credits
+
+import org.axonframework.eventsourcing.eventstore.EventStore
+import org.karamelsoft.axon.bank.interfaces.http.Event
+import org.karamelsoft.axon.bank.services.credits.api.CreditLineId
+import org.karamelsoft.research.axon.libraries.artifacts.module.readEvents
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Flux
+
+@RestController
+@RequestMapping("/credits")
+class CreditsController(val eventStore: EventStore) {
+
+    @GetMapping("/{creditLineId}/history")
+    fun getHistory(creditLineId: String): Flux<Event> {
+        return eventStore.readEvents(CreditLineId.from(creditLineId))
+            .map { domainEvent -> Event.from(domainEvent.payload) }
+    }
+
+}
